8b42ba1aaf5446133a28d4699e04198978314708
Dominic Tillison <22tilldj@gmail.com>
2025-09-19 11:48:34 -0400

    post note is working, and its showing correctly, however its still throwing a 500 error code

--- src/components/services/AllPostServices.jsx
+++ src/components/services/AllPostServices.jsx
@@ -1,7 +1,7 @@
-import { fetchWithoutResponse, fetchWithResponse } from "./fetcher.js"
+import { fetchWithoutResponse, fetchWithResponse } from "./fetcher.js";
 
 export const getAllPost = (id = undefined) => {
-try {
+  try {
     let url = "posts";
 
     // If an id is provided, fetch a single user
@@ -11,14 +11,14 @@ try {
 
     const stored = localStorage.getItem("bible_user");
     const parsed = stored ? JSON.parse(stored) : null;
-    const token = parsed?.token; 
-    console.log(token)
+    const token = parsed?.token;
+    console.log(token);
 
     return fetchWithResponse(url, {
       headers: {
         Authorization: `Token ${token}`,
-      }
-    })
+      },
+    });
   } catch (error) {
     console.error("Error fetching packages:", error);
     return []; // optional fallback for logged-out users
@@ -26,13 +26,13 @@ try {
 };
 
 export const createCustomPost = (post) => {
-try {
+  try {
     let url = "posts";
 
     const stored = localStorage.getItem("bible_user");
     const parsed = stored ? JSON.parse(stored) : null;
-    const token = parsed?.token; 
-    console.log(token)
+    const token = parsed?.token;
+    console.log(token);
 
     return fetchWithResponse(url, {
       method: "POST",
@@ -40,14 +40,13 @@ try {
         "Content-Type": "application/json",
         Authorization: `Token ${token}`,
       },
-      body: JSON.stringify(post)
-    })
-  }
-  catch (error) {
+      body: JSON.stringify(post),
+    });
+  } catch (error) {
     console.error("Error fetching packages:", error);
     return []; // optional fallback for logged-out users
   }
-  }
+};
 
 export const createUpdatePost = (post, postId) => {
   return fetch(`https://capstone-2025-dominic-3.onrender.com/posts/${postId}`, {
@@ -92,27 +91,40 @@ export const postComments = (comment) => {
 };
 
 export const saveUpdatedNote = (note) => {
-  return fetch("https://capstone-2025-dominic-3.onrender.com/notes", {
+  let url = "notes";
+
+  const stored = localStorage.getItem("bible_user");
+  const parsed = stored ? JSON.parse(stored) : null;
+  const token = parsed?.token;
+
+  return fetchWithResponse(url, {
     method: "POST",
     headers: {
+      Authorization: `Token ${token}`,
       "Content-Type": "application/json",
     },
     body: JSON.stringify(note),
   });
 };
 
-export const deleteNote = (id) => {
-  return fetch(`https://capstone-2025-dominic-3.onrender.com/notes/${id}`, {
-    method: "DELETE"
-  })
-}
+export const deleteNote = (id = undefined) => {
+  let url = "notes";
+
+  if (id) {
+    url += `/${id}`;
+  }
+
+  return fetchWithResponse(url, {
+    method: "DELETE",
+  });
+};
 
 export const createUserLike = (obj) => {
   return fetch(`https://capstone-2025-dominic-3.onrender.com/userLikes`, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(obj)
-  })
-}
+    body: JSON.stringify(obj),
+  });
+};
